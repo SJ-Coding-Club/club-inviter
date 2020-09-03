4f8f8531ac9bc9f7855872132072a5a366f9daa6
Jack Donofrio <johndonofrio2021@students.sjcadets.org>
2020-09-03 13:01:14 -0400

    Initial commit

---
+++ inviter.py
@@ -0,0 +1,10 @@
+from github import Github
+import os
+g = Github(os.environ.get('GITHUB_EMAIL'),os.environ.get('GITHUB_PASSWORD'))
+club = g.get_organization('SJ-Coding-Club')
+for issue in club.get_repo('sj-coding-club.github.io').get_issues():
+    if 'add me' in issue.title.lower():
+        if issue.user.login not in [x.login for x in club.get_members()]:
+            club.invite_user(user=issue.user)
+        else:
+            issue.edit(state='closed')
